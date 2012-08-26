5af1875a5aa7bb98451fff14ea1d65949e734c0f
tbotaq <tbotaq@gmail.com>
2012-08-26 22:15:06 +0900

    compressed js files

---
+++ app/webroot/js/unpacked/admin.js
@@ -0,0 +1,80 @@
+$(function(){
+
+  // admin screen
+  var admin = $("#wrap-admin");
+  
+  // ----
+
+  // click event handler for the button to delete each single account
+  admin.find(".delete-each").click(function(){
+
+    var parent = $(this).parent();
+
+    // show the loader
+    showLoader(parent);
+
+    // check which user id does clicked button point
+    var dest_id = $(this).data('dest-id');
+    deleteHim(dest_id);
+    
+    
+  });
+                                  
+  // click event handler for the button to delete selected accounts 
+  admin.find("#delete-selected").click(function(){
+
+    var parent = $(this).parent();
+
+    // show the loader
+    showLoader(parent);
+    
+    // array to contain the destination ids
+    var dest_id;
+    
+    var checked_buttons = admin.find("tbody tr .chk button.active");
+  
+    if(checked_buttons.length > 0){
+        
+      checked_buttons.each(function(i,element){
+        
+        dest_id = $(element).data("dest-id");
+        
+        deleteHim(dest_id);
+
+      },hideLoader(parent));;
+
+    }else{
+      
+      // no button is selected 
+      alert("nothing is selected");
+      hideLoader(".wrap-button");
+    }
+
+  });
+
+});
+
+function deleteHim(dest_id){
+  
+  var admin = $("#wrap-admin");
+
+  $.ajax({
+    url:"/ajax/delete_him",
+    type:"post",
+    data:{"dest_id":dest_id},
+    dataType: "text",
+    success: function(res){
+      if(res == "NG"){
+        
+        alert("something went wrong with deleting account");
+      }else{
+        // hide the element of deleted account
+        admin.find("tr[data-dest-id="+dest_id+"]").fadeOut();
+      }
+    },
+    error: function(){
+
+      alert("Ajax error");
+    }
+  });
+}
---
+++ app/webroot/js/unpacked/popstate.js
@@ -0,0 +1,59 @@
+$(function(){
+
+  /////////////////////////////
+  // code for popstate event //
+  /////////////////////////////
+
+  if('pushState' in history){
+
+    window.setTimeout(function(){
+      $(window).bind("popstate",function(e){
+        
+        var white_list = ['tweets','home_timeline','public_timeline'];
+        var path = location.pathname;
+        
+        var actionTypeOk = false;
+        var slashCountOk = false;
+        
+        // check if requested action type is allowed to fire process on popstate
+        for(var i=0;i<white_list.length;i++){
+          
+          if(countStr(path,white_list[i]) > 0){
+            actionTypeOk = true;
+            break;
+          }
+          
+        }
+        
+        // check if displaying screen is showing error
+        // [ToDo]
+        
+        // check if requested path contians 3 slashes
+        /*
+          if(countStr(path,"/") == 3){
+          slashCountOk = true;
+          }
+        */    
+        
+        if( actionTypeOk ){
+          var date;
+          if(countStr(path,"/") < 3){
+            date = "notSpecified";
+          }else{
+            date = detectDate(path);
+          }
+          
+          var action_type = detectActionType(path);
+          
+          ajaxSwitchTerm(date,action_type,"pjax");
+          
+          // reset all the term selectors
+          $("#wrap-term-selectors").find("a.selected").removeClass("btn-primary selected");
+        }
+        
+      });
+    },1000);
+    
+  }
+  
+});
