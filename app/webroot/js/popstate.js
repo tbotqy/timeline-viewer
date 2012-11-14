3c05039d0f58b2bd773d4407aeb9f34227f8bec5
tbotaq <tbotaq@gmail.com>
2012-11-14 21:45:44 +0900

    compressed js

--- app/webroot/js/popstate.js
+++ app/webroot/js/popstate.js
@@ -1,1 +1,7 @@
-$(function(){if('pushState'in history){window.setTimeout(function(){$(this).on("popstate",function(e){var a=['tweets','home_timeline','public_timeline'];var b=location.pathname;var c=false;var d=false;for(var i=0;i<a.length;i++){if(countStr(b,a[i])>0){c=true;break}}if(c){var f;var g=b.indexOf("public_timeline")!=-1;var h;if(g){h=2}else{h=3}if(countStr(b,"/")<h){f="notSpecified"}else{f=detectDate(b)}var j=detectActionType(b);ajaxSwitchTerm(f,j,"pjax");$("#wrap-term-selectors").find("a.selected").removeClass("btn-primary selected")}})},1000)}});
+$(function(){ if('pushState' in history){ window.setTimeout(function(){ $(this).on("popstate",function(e){ var white_list = ['tweets','home_timeline','public_timeline']; var path = location.pathname; var actionTypeOk = false; var slashCountOk = false; for(var i=0;i<white_list.length;i++){ if(countStr(path,white_list[i]) > 0){ actionTypeOk = true; break;}
+}
+if( actionTypeOk ){ var date; var isPublicTimeline = path.indexOf("public_timeline") != -1; var threshold; if(isPublicTimeline){ threshold = 2;}else{ threshold = 3;}
+if(countStr(path,"/") < threshold){ date = "notSpecified";}else{ date = detectDate(path);}
+var action_type = detectActionType(path); ajaxSwitchTerm(date,action_type,"pjax"); $("#wrap-term-selectors").find("a.selected").removeClass("btn-primary selected");}
+});},2000);}
+}); 
